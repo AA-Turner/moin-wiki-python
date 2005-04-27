75d41a406fd577e69f31d24f1ff77ccb3f892256
anonymous <anonymous@example.org>
2005-04-27 23:44:04 +0000

    Attach lecture4_but.py to Intro to programming with Python and Tkinter.txt

---
+++ moin/_attachments/Intro to programming with Python and Tkinter/lecture4_but.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from Tkinter import *
+
+
+def Pressed():				#function
+	print 'buttons are cool'
+
+
+root = Tk()				#main window
+
+button = Button(root, text = 'Press', command = Pressed)
+
+button.pack(pady=20, padx = 20)
+Pressed()
+
+root.mainloop()
+
